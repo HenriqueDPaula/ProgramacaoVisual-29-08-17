938f7414a9cfb641516869ed8baacf87f9762983
Tiago Oliveira <tiago@econtabil.com>
2017-08-29 21:58:34 -0300

    Ajustado Category para persistir no banco de dados, e nao em listas

--- WebApplication1/Controllers/CategoriesController.cs
+++ WebApplication1/Controllers/CategoriesController.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
+using WebApplication1.Content;
+using System.Data.Entity;
+using System.Net;
 
 namespace WebApplication1.Controllers
 {
     public class CategoriesController : Controller
     {
-        private static IList<Category> categoryList =
+        private readonly EFContext _context = new EFContext();
+        /*private static IList<Category> categoryList =
             new List<Category>()
             {
                 new Category() { CategoryId = 1, Name = "Keyboard"},
@@ -18,11 +22,14 @@ namespace WebApplication1.Controllers
                 new Category() { CategoryId = 4, Name = "Mouse"},
                 new Category() { CategoryId = 5, Name = "Printers"}
             };
-        
+        */
         // GET: Categories
         public ActionResult Index()
         {
-            return View(categoryList.OrderBy(c => c.Name) );
+            return View(_context
+                .Categories
+                .OrderBy(s => s.Name));
+            //return View(categoryList.OrderBy(c => c.Name) );
         }
 
         public ActionResult Create()
@@ -34,63 +41,118 @@ namespace WebApplication1.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Category category)
         {
-            categoryList.Add(category);
-            category.CategoryId = categoryList.Max(c => c.CategoryId) + 1;
-            return RedirectToAction("Create");
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        public ActionResult Detail(long id)
+        public ActionResult Detail(long? id)
         {
-            var category = categoryList
+            /*var category = categoryList
                 .Where(c => c.CategoryId == id)
-                .First();
+                .First();*/
 
             /* A função acima poderia ser escrita conforme abaixo
              * category = categoryList
                 .First(c => c.CategoryId == id);
                 */
+
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.BadRequest);
+            }
+
+            var category = _context
+                .Categories
+                .Find(id.Value);
+
+            if (category == null)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.NotFound);
+            }
+
             return View(category);
         }
 
-        public ActionResult Edit(long id)
+        public ActionResult Edit(long? id)
         {
-            var category = categoryList
-                .Where(c => c.CategoryId == id)
-                .First();
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.BadRequest);
+            }
+
+            var category = _context
+                .Categories
+                .Find(id.Value);
+
+            if (category == null)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.NotFound);
+            }
+
             return View(category);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(Category modified)
+        public ActionResult Edit(Category category)
         {
-            var category = categoryList
-                .Where(c => c.CategoryId == modified.CategoryId)
-                .First();
-
-            category.Name = modified.Name;
+            if (ModelState.IsValid)
+            {
+                _context.Entry(category)
+                    .State = EntityState.Modified;
+                _context.SaveChanges();
+            }
 
             return RedirectToAction("Index");
         }
 
-        public ActionResult Delete(long id)
+        public ActionResult Delete(long? id)
         {
-            var category = categoryList
-                .Where(c => c.CategoryId == id)
-                .First();
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.BadRequest);
+            }
+
+            var category = _context
+                .Categories
+                .Find(id.Value);
+
+            if (category == null)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.NotFound);
+            }
+
             return View(category);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(Category toDelete)
+        public ActionResult Delete(Category category)
         {
-            var category = categoryList
-                .Where(c => c.CategoryId == toDelete.CategoryId)
-                .First();
-            categoryList.Remove(category);
+            if (ModelState.IsValid)
+            {
+                var s = _context
+                    .Categories
+                    .Find(category.CategoryId);
 
-            return RedirectToAction("Index");
+                if (s == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+                _context.Categories.Remove(s);
+
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
         }
 
     }
--- WebApplication1/Controllers/SuppliersController.cs
+++ WebApplication1/Controllers/SuppliersController.cs
@@ -80,5 +80,80 @@ namespace WebApplication1.Controllers
         }
         #endregion
 
+        #region details
+        public ActionResult Details(long? id)
+        {
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.BadRequest);
+            }
+
+            var supplier = _context
+                .Suppliers
+                .Find(id.Value);
+
+            if (supplier == null)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.NotFound);
+            }
+
+            return View(supplier);
+
+        }
+
+        #endregion
+
+        #region delete
+        public ActionResult Delete(long? id)
+        {
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.BadRequest);
+            }
+
+            var supplier = _context
+                .Suppliers
+                .Find(id.Value);
+
+            if (supplier == null)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.NotFound);
+            }
+
+            return View(supplier);
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Delete(Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                var s = _context
+                    .Suppliers
+                    .Find(supplier.SupplierId);
+
+                if (s == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+                _context.Suppliers.Remove(s);
+
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(supplier);            
+                
+        }
+        #endregion
+
     }
 }
